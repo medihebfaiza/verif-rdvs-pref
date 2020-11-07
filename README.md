b8da27a99bd90302cd83cc67ddd02efaf7b4554f
Mohamed Iheb Faiza <fmohamediheb@gmail.com>
2020-11-07 10:38:07 +0100

    🎉 first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # verif-rdvs-pref
-bot permettant de vérifier les disponibilités des rdvs sur le site de la préfecture et d’émettre des notifications 
+![](https://image.flaticon.com/icons/png/128/120/120567.png)![](https://www.herault.gouv.fr/var/ide_site/storage/images/design/dans-l-herault/166055-37-fre-FR/dans-l-Herault.png)
+Un bot permettant de vérifier les disponibilités des rdvs sur [le site de la préfecture de l'hérault](http://www.herault.gouv.fr/booking/create/15253/0) et d’émettre des notifications.
+
+
+# Installation
+Requires `Python 3`.
+1. Run : 
+```bash
+pip install -r requirements.txt
+```
+2. Download the chrome driver corresponding to your chrome version : https://chromedriver.chromium.org/downloads
+3. Save the path to the chrome driver in a `chrome_driver.txt` file.
+
+# Configuration
+1. Run :
+```bash
+notify-run register
+```
+
+1. Save the endpoint in a `notify_endpoint.txt` file.
+2. Open the endpoint with a browser and subscribe for notifications.
+
+# Launch 
+Run :
+```bash
+python  pref.py
+```
---
+++ pref.py
@@ -0,0 +1,41 @@
+import requests 
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+from notify_run import Notify
+
+
+pref_url = 'https://www.herault.gouv.fr/booking/create/15253/0'
+chrome_driver = open("chrome_driver.txt","r").read()
+notify_endpoint = open("notify_endpoint.txt","r").read()
+
+print(chrome_driver)
+print(notify_endpoint)
+
+check_every = 10
+
+def bypass502(driver): 
+  if driver.find_elements_by_xpath("//*[text()='502 Bad Gateway']"):
+    while driver.find_elements_by_xpath("//*[text()='502 Bad Gateway']"):
+      driver.refresh()
+      sleep(5)
+
+requests.post(notify_endpoint, {'message': 'La vérification des RDVs est lancée', 'action': None})
+
+driver = webdriver.Chrome(chrome_driver)
+driver.get(pref_url)
+bypass502(driver)
+driver.find_element_by_link_text("Accepter").click()
+while True:
+  bypass502(driver)
+  driver.find_element_by_name("condition").click()
+  driver.find_element_by_name("nextButton").click()
+  bypass502(driver)
+  if driver.find_elements_by_name("finishButton") :
+    driver.find_element_by_name("finishButton").click()
+    sleep(check_every * 60)
+  else:
+    requests.post(notify_endpoint, {'message': 'Des RDVs sont disponibles sur le site de la préfecture', 'action': None})
+
+
+
